c35734f4ba57244fb4f3a87da6a1857fd24cf469
OlofLindber9 <oloflind@student.chalmers.se>
2025-05-28 23:55:20 +0200

    made homepage green

--- app/page.tsx
+++ app/page.tsx
@@ -16,30 +16,41 @@ export default function Home() {
             <Link
               key={item.href}
               href={item.href}
-              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+              className="text-sm duration-500"
+              style={{ color: '#00795a' }}
             >
               {item.name}
             </Link>
           ))}
         </ul>
       </nav>
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-[#00a479]/0 via-[#00a479]/50 to-transparent" />
       <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        Mallard Software
-      </h1>
+      <div className="flex flex-col items-center z-10 animate-fade-in">
+      <img
+        src="/mallard_logo_wide.png"
+        alt="Mallard Software Logo"
+        className="max-w-2xl w-full mx-auto mb-4"
+      />
+        <h1
+          className="py-3.5 px-0.5 text-4xl z-10 duration-1000 cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap"
+          style={{ color: '#00a479' }}
+        >
+          Mallard Software
+        </h1>
+      </div>
 
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-[#00a479]/0 via-[#00a479]/50 to-transparent" />
       <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">
+        <h2 className="text-sm" style={{ color: '#00795a' }}>
           I'm building{" "}
           <Link
             target="_blank"
             href="https://unkey.dev"
-            className="underline duration-500 hover:text-zinc-300"
+            className="underline duration-500 hover:text-[#00a479]"
           >
             unkey.dev
           </Link> to solve API authentication and authorization for developers.
