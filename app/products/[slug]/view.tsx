6f147dbf8c3e2e358244a9fe297ae98fc324f023
OlofLindber9 <oloflind@student.chalmers.se>
2025-05-30 02:01:36 +0200

    fixed empty slug files

--- app/products/[slug]/view.tsx
+++ app/products/[slug]/view.tsx
@@ -1,1 +1,8 @@
- 
+"use client";
+
+import { useEffect } from "react";
+
+export const ReportView: React.FC<{ slug: string }> = ({ slug }) => {
+	// No-op since we're using mock data
+	return null;
+};
